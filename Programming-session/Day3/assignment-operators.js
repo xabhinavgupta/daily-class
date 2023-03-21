517fe596031398c5fe49d4cb692c9f72ac767aa5
Abhinav Gupta <abhi.mechkiit@gmail.com>
2023-03-21 08:06:56 +0530

    Responsive page with toggle

---
+++ Programming-session/Day3/assignment-operators.js
@@ -0,0 +1,22 @@
+let a = 10;
+console.log(a);
+
+a = a + 10;
+console.log(a);
+
+a += 40;
+console.log(a);
+
+ a *= 2;
+ console.log(a);
+
+ a **=3;
+ console.log(a);
+
+ a **=(1/2);
+ console.log(a);
+
+ a **= 2;
+ console.log(a);
+
+ 
